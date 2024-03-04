cbb7d791f4b5e0b556f906f786987b5db910604a
Christoffer <christofferfriman@Christoffer-Friman-macbook.local>
2024-03-04 22:16:33 +0100

    scss justering

--- src/Components/Puzzle.jsx
+++ src/Components/Puzzle.jsx
@@ -34,7 +34,7 @@ function Puzzle() {
   };
 
   return (
-    <Container className="position-relative">
+    <Container className="position-relative custom-style">
       <StatusMessage status={gameStatus} time={time} />
       <div className="board-container">
         <Board numbers={numbers} onTileClick={moveTileOnClick} />
